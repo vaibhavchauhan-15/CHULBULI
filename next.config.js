26ef517f8913efdaec4cd888b21af59c7a761133
vaibhavchauhan-15 <vaibhavchauhan12353@gmail.com>
2026-02-07 15:34:28 +0530

    feat: add jsconfig.json for TypeScript configuration and update next.config.js for path aliasing

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './src'),
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
